a22f95e1dde8dcde285bb282b89130104525abec
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-25 09:36:06 +0530

    added string slicing

---
+++ 001_string_slicing.py
@@ -0,0 +1,34 @@
+# STRING SLICING
+
+my_string = "Hello this is ENTC students"
+
+# first letter access
+print(my_string[0])
+
+# access second letter
+print(my_string[1])
+
+# access 'Hello' word
+print(my_string[0:5])
+
+# access h'Hlo'
+print(my_string[0:5:2])
+
+# access last letter
+print(my_string[-1])
+
+# second last letter 
+print(my_string[-2])
+
+# reverse string
+print(my_string[::-1])
+
+# reverse students
+print(my_string[:-9:-1])
+
+# upper/lower case
+print(my_string.upper())
+print(my_string.lower())
+
+# replace
+print(my_string.replace('s', 'X'))
