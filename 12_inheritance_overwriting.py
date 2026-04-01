79edaf042bea3aa1782d8306566a41af41aac958
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-04-01 10:36:37 +0530

    function overwriting

---
+++ 12_inheritance_overwriting.py
@@ -0,0 +1,22 @@
+class maths:
+
+    def __init__(self, first_num, second_num):
+        self.first_num = first_num
+        self.second_num = second_num
+
+    def addition(self):
+        return self.first_num + self.second_num
+    
+class additionOperation(maths):
+
+    def __init__(self, first_num, second_num):
+        maths.__init__(self, first_num, second_num)
+
+    def addition(self):
+        return self.first_num * self.second_num
+
+# mathsIns = maths(2, 3)
+# print(mathsIns.addition())
+
+additionOperationIns = additionOperation(2, 3)
+print(additionOperationIns.addition())
